ace443dd4d0978187b85b680f37c35706f4c1050
dahyunA <baha63985@gmail.com>
2025-04-19 23:35:49 +0900

    Update StudGrade.java

--- StudGrade.java
+++ StudGrade.java
@@ -4,7 +4,7 @@ public class StudGrade {
 
         int[] scores = {85, 92, 78, 65, 55, 90, 73, 88, 47, 100};
         int sum = 0;
-        int count = 0;
+        int count = 0;    // score로 사용한 부분 모두 대체 가능!
         int max = 0;
         int min = 100;
 
@@ -28,7 +28,7 @@ public class StudGrade {
             System.out.println("학생 " + (i + 1) + ": 성적 = " + score + ", 평가 = " + grade + " " + result);
 
             sum += score;
-            count++;
+            //count++;
 
             if(score > max) {
                 max = score;
@@ -44,7 +44,7 @@ public class StudGrade {
             }
         }
         if(count > 0) {
-            double avg = (double) sum/count;
+            double avg = (double) sum/count;    // (double)sum/scores.length
             System.out.println("평균 점수 : " + avg);
             System.out.println("최고 점수 : " + max);
             System.out.println("최저 점수 : " + min);
